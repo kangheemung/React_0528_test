37c989d3c90868207f7a6bee1b21b47a2d4aa7e7
EC2 Default User <ec2-user@ip-172-31-46-110.ap-northeast-1.compute.internal>
2024-05-29 15:01:56 +0000

    test

---
+++ src/components/Sidebaricon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "../images/unibers.png";
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} />
+      <p>programming@gmail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
